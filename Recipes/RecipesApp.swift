8f9a2d8ebd57d996428b996509f9f6be67dde586
Avaneesh Pareek <pareekavi10@gmail.com>
2023-04-15 18:43:57 +0530

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Avaneesh on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
